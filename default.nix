a29b1190d72834f5e51d64d1cbb344047b1584be
Yorick van Pelt <yorick@yorickvanpelt.nl>
2019-08-16 15:01:17 +0200

    add default.nix

---
+++ default.nix
@@ -0,0 +1,19 @@
+let
+  sources = import (./nix/sources.nix);
+  nixpkgs = import sources.nixpkgs {
+    overlays = [ (import "${sources.serokell-closure}/pkgs") ];
+  };
+  stackToNix = nixpkgs.callPackage sources.stack-to-nix { };
+  hlib = nixpkgs.haskell.lib;
+in (stackToNix {
+  root = nixpkgs.constGitIgnore "crossref-verifier" ./. [ ];
+  overrides = (final: previous: {
+    tasty-hedgehog = null;
+    tiempo = null;
+    time-units = null;
+    crossref-verifier = hlib.overrideCabal previous.crossref-verifier (o: {
+      buildTools = (o.buildTools or [ ]) ++ [ final.hspec-discover ];
+      doHaddock = false;
+    });
+  });
+}).crossref-verifier

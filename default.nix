64c2b08d8fde3a8af03cf5947c81095d3022a814
Alexander Bantyev <balsoft75@gmail.com>
2019-11-27 12:25:44 +0300

    Switch to haskell.nix
    Co-Authored-By: Yorick van Pelt <yorick@yorickvanpelt.nl>

--- default.nix
+++ default.nix
@@ -1,18 +1,17 @@
 let
-  sources = import (./nix/sources.nix);
-  nixpkgs = import sources.nixpkgs {
-    overlays = [ (import "${sources.serokell-closure}/pkgs") ];
-  };
-  hlib = nixpkgs.haskell.lib;
-in (nixpkgs.stackToNix {
-  root = nixpkgs.constGitIgnore "crossref-verifier" ./. [ ];
-  overrides = (final: previous: {
-    tasty-hedgehog = null;
-    tiempo = null;
-    time-units = null;
-    crossref-verifier = hlib.overrideCabal previous.crossref-verifier (o: {
-      buildTools = (o.buildTools or [ ]) ++ [ final.hspec-discover ];
-      doHaddock = false;
-    });
-  });
+  sources = import ./nix/sources.nix;
+  nixpkgs = import sources.nixpkgs
+    (import sources."haskell.nix");
+  hn = nixpkgs.haskell-nix;
+in (hn.stackProject {
+  src = hn.haskellLib.cleanGit { src = ./.; };
+  cache = [
+    {
+      name = "loot-prelude";
+      rev = "34e389808e34f1cfa56f456773682325bed56d17";
+      sha256 = "19b51x0yml13wpvj0b967vgg5y62av94izjr9sfwxr6sww2wgk29";
+      url = "https://github.com/serokell/lootbox.git";
+      subdir = "code/prelude";
+    }
+  ];
 }).crossref-verifier

87f66c44d4f5a6a5dd3f94d0b7906c77b41f178e
Alexander Bantyev <balsoft75@gmail.com>
2019-12-03 23:26:15 +0300

    Use niv for cache

--- default.nix
+++ default.nix
@@ -4,11 +4,12 @@ let
   hn = nixpkgs.haskell-nix;
 in (hn.stackProject {
   src = hn.haskellLib.cleanGit { src = ./.; };
-  cache = [{
-    name = "loot-prelude";
-    rev = "4700376b8493f6ac164461715cb72a0259148ac2";
-    sha256 = "0yh1xi9p7ky5bm40pqjs0ygxqcbds226sj481r1cagnk0lp5vl3f";
-    url = "https://github.com/serokell/lootbox";
-    subdir = "code/prelude";
-  }];
+  cache = with sources; [
+    {
+      name = "loot-prelude";
+      inherit (lootbox) sha256 rev;
+      url = "https://github.com/${lootbox.owner}/${lootbox.repo}.git";
+      subdir = "code/prelude";
+    }
+  ];
 }).crossref-verifier

469e7b8e3b4877aa065746150e37d26a15caadc3
Alexander Bantyev <balsoft75@gmail.com>
2019-12-05 00:48:31 +0300

    Fail if haddock is not generated

--- default.nix
+++ default.nix
@@ -4,12 +4,17 @@ let
   hn = nixpkgs.haskell-nix;
 in (hn.stackProject {
   src = hn.haskellLib.cleanGit { src = ./.; };
-  cache = with sources; [
-    {
-      name = "loot-prelude";
-      inherit (lootbox) sha256 rev;
-      url = "https://github.com/${lootbox.owner}/${lootbox.repo}.git";
-      subdir = "code/prelude";
-    }
-  ];
+  modules = [{
+    packages.crossref-verifier = {
+      # More failures during CI == Less failures in runtime!
+      postHaddock = ''[[ -z "$(ls -A dist/doc/html)" ]] && exit 1 || echo "haddock successfully generated documentation"'';
+      package.ghcOptions = "-Werror";
+    };
+  }];
+  cache = with sources; [{
+    name = "loot-prelude";
+    inherit (lootbox) sha256 rev;
+    url = "https://github.com/${lootbox.owner}/${lootbox.repo}.git";
+    subdir = "code/prelude";
+  }];
 }).crossref-verifier

5fb8841df876da06e66cb7cdcd24c1ce9b477359
Alexander Bantyev <balsoft75@gmail.com>
2019-12-05 18:21:34 +0300

    Also build tests

--- default.nix
+++ default.nix
@@ -2,19 +2,21 @@ let
   sources = import ./nix/sources.nix;
   nixpkgs = import sources.nixpkgs (import sources."haskell.nix");
   hn = nixpkgs.haskell-nix;
-in (hn.stackProject {
-  src = hn.haskellLib.cleanGit { src = ./.; };
-  modules = [{
-    packages.crossref-verifier = {
-      # More failures during CI == Less failures in runtime!
-      postHaddock = ''[[ -z "$(ls -A dist/doc/html)" ]] && exit 1 || echo "haddock successfully generated documentation"'';
-      package.ghcOptions = "-Werror";
-    };
-  }];
-  cache = with sources; [{
-    name = "loot-prelude";
-    inherit (lootbox) sha256 rev;
-    url = "https://github.com/${lootbox.owner}/${lootbox.repo}.git";
-    subdir = "code/prelude";
-  }];
-}).crossref-verifier.components.exes
+  project = hn.stackProject {
+    src = hn.haskellLib.cleanGit { src = ./.; };
+    modules = [{
+      packages.crossref-verifier = {
+        # More failures during CI == Less failures in runtime!
+        postHaddock = ''
+          [[ -z "$(ls -A dist/doc/html)" ]] && exit 1 || echo "haddock successfully generated documentation"'';
+        package.ghcOptions = "-Werror";
+      };
+    }];
+    cache = with sources; [{
+      name = "loot-prelude";
+      inherit (lootbox) sha256 rev;
+      url = "https://github.com/${lootbox.owner}/${lootbox.repo}.git";
+      subdir = "code/prelude";
+    }];
+  };
+in with project; [ crossref-verifier.components.exes crossref-verifier ]

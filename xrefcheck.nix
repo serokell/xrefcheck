442554d41ab25116654593f78f8247a39c64af33
Yorick van Pelt <yorick@yorickvanpelt.nl>
2020-03-09 15:53:00 +0100

    xrefcheck.nix: bump hs.nix, remove unneccesary flags
    https://github.com/input-output-hk/haskell.nix/commit/775c8cf756c08a779bbc587013c27f4f4b3df71a
    fixed a bunch of problems with musl, we can now simplify and only need
    the custom zlib.

--- xrefcheck.nix
+++ xrefcheck.nix
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2019 Serokell <https://serokell.io>
+# SPDX-FileCopyrightText: 2020 Serokell <https://serokell.io>
 #
 # SPDX-License-Identifier: MPL-2.0
 
@@ -6,30 +6,15 @@
 let
   sources = import ./nix/sources.nix;
   nixpkgs = import sources.nixpkgs (import sources."haskell.nix");
-  hn = if static then
-    nixpkgs.pkgsCross.musl64.haskell-nix
-  else
-    nixpkgs.haskell-nix;
-  project = hn.stackProject {
-    src = hn.haskellLib.cleanGit { src = ./.; };
-    modules = [
-      {
+  pkgs = if static then nixpkgs.pkgsCross.musl64 else nixpkgs;
+  project = pkgs.haskell-nix.stackProject {
+    src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
+    modules = [{
       packages.xrefcheck = {
         package.ghcOptions = "-Werror";
-        configureFlags =
-          with nixpkgs.pkgsStatic;
+        configureFlags = with pkgs;
           lib.optionals static [
-            "--disable-executable-dynamic"
-            "--disable-shared"
-            "--ghc-option=-optl=-pthread"
-            "--ghc-option=-optl=-static"
-            "--ghc-option=-optl=-lc"
-            "--ghc-option=-optl=-lz"
-            "--ghc-option=-optl=-lgmp"
-            "--ghc-option=-optl=-lffi"
-            "--ghc-option=-optl=-L${gmp6}/lib"
             "--ghc-option=-optl=-L${zlib.static}/lib"
-            "--ghc-option=-optl=-L${libffi}/lib"
           ];
       };
     }];

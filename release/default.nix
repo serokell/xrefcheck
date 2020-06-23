771d0abe42ea6c2f824588dfebd2b17e58b06106
Alexander Bantyev <balsoft@balsoft.ru>
2020-06-24 01:14:45 +0300

    Adapt release/default.nix to work with new release-binary script

--- release/default.nix
+++ release/default.nix
@@ -2,19 +2,21 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-{ pkgs ? import (import ../nix/sources.nix).nixpkgs { } }:
+{ pkgs ? import (import ../nix/sources.nix).nixpkgs { }, timestamp }:
 let
-  executable = (import ../xrefcheck.nix {
-    static = true;
-  }).components.exes.xrefcheck;
-  joinByBasename = name: paths:
-    pkgs.runCommandNoCC name { } (''
-      mkdir $out
-    '' + pkgs.lib.concatMapStrings (path: ''
-      ln -s ${path} $out/${builtins.baseNameOf path}
-    '') paths);
-in joinByBasename "xrefcheck-release" [
-  "${executable}/bin"
-  ../LICENSES
-  ../README.md
+  executable =
+    (import ../xrefcheck.nix { static = true; }).components.exes.xrefcheck;
+
+  releaseNotes = pkgs.writeText "release-notes.md" ''
+    Automatic release on ${timestamp}
+  '';
+in pkgs.linkFarm "xrefcheck-release" [
+  {
+    name = "release-notes.md";
+    path = releaseNotes;
+  }
+  {
+    name = "xrefcheck-x86_64-linux";
+    path = "${executable}/bin/xrefcheck";
+  }
 ]

a0b3f9a5ad9ba9fa6b75d2718626c5cb46a12772
Zhenya Vinogradov <zhenyavinogradov@gmail.com>
2021-03-11 14:37:54 +0300

    Add trailing whitespace check to CI
    The check is imported from serokell.nix. Nix files have been refactored,
    to make 'cleanGit' funcion available in ci.nix

--- ci.nix
+++ ci.nix
@@ -4,9 +4,26 @@
 
 rec {
   sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
+  haskell-nix = import sources."haskell.nix" {
+    sourcesOverride = { hackage = sources."hackage.nix"; stackage = sources."stackage.nix"; };
+  };
+  serokell-nix = import sources."serokell.nix";
+  pkgs = import sources.nixpkgs (
+    haskell-nix.nixpkgsArgs // {
+      overlays =
+        haskell-nix.nixpkgsArgs.overlays
+        ++ [ serokell-nix.overlay ]; # contains trailing whitespace check
+    }
+  );
+
+  project-src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "xrefcheck";
+    src = ./.;
+  };
 
   xrefcheck-lib-and-tests = (import ./xrefcheck.nix { linux = true; });
   xrefcheck-static = (import ./xrefcheck.nix { linux-static = true; }).components.exes.xrefcheck;
   xrefcheck-windows = (import ./xrefcheck.nix { windows = true; }).components.exes.xrefcheck;
+
+  trailing-whitespace-check = pkgs.build.checkTrailingWhitespace project-src;
 }

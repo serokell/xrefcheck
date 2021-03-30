9d0037b7a2a7511e178f56d9b40db3c64bd74425
Alexander Bantyev <balsoft@balsoft.ru>
2021-03-30 15:19:19 +0300

    Add licensing information to scripts/upload-docker-image.sh

--- scripts/upload-docker-image.sh
+++ scripts/upload-docker-image.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+# SPDX-FileCopyrightText: 2019 Serokell <https://serokell.io>
+#
+# SPDX-License-Identifier: MPL-2.0
+
 skopeo --insecure-policy copy --dest-creds "serokell:${DOCKERHUB_PASSWORD}" "$1" "$2"

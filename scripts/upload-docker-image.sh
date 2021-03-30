aee39ef06edc0c41655e5809e1409997ce140755
Alexander Bantyev <balsoft@balsoft.ru>
2021-03-30 17:15:20 +0300

    Fix docker image upload (#63)
    * Fix docker image upload
    
    The previous way of uploading images to dockerhub exposed our password.
    Prevent this from happening by using a separate script instead of calling
    skopeo directly.
    
    Co-authored-by: Zhenya Vinogradov <zhenyavinogradov@gmail.com>

---
+++ scripts/upload-docker-image.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+skopeo --insecure-policy copy --dest-creds "serokell:${DOCKERHUB_PASSWORD}" "$1" "$2"

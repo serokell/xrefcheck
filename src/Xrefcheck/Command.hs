5e266775bdc2a1f009fcb1a831d3e2a3017f0e56
Andrey Demidenko <andrey.demidenko@serokell.io>
2021-09-22 18:05:29 +0300

    [Chore] Move commands related functionality to lib
    Problem:
    We have `exec` directory with `Main.hs` which (from semantic) supposed
    to only run Xrefcheck.
    Also, it's a common practice to keep `Main.hs` as simple as it possible.
    But now it have some logic.
    
    Solution:
    Move commands related functionality to lib (separate module) and keep
    `Main.hs` very simple, only as runner.

---
+++ src/Xrefcheck/Command.hs
@@ -0,0 +1,75 @@
+{- SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io>
+ -
+ - SPDX-License-Identifier: MPL-2.0
+ -}
+
+module Xrefcheck.Command
+  ( defaultAction
+  ) where
+
+import Data.Yaml (decodeFileEither, prettyPrintParseException)
+import Fmt (blockListF', build, fmt, fmtLn, indentF)
+import System.Directory (doesFileExist)
+
+import Xrefcheck.CLI (Options (..), addTraversalOptions, addVerifyOptions, defaultConfigPaths)
+import Xrefcheck.Config (Config (..), ScannersConfig (..), defConfig)
+import Xrefcheck.Core (Flavor (..))
+import Xrefcheck.Progress (allowRewrite)
+import Xrefcheck.Scan (FormatsSupport, gatherRepoInfo, specificFormatsSupport)
+import Xrefcheck.Scanners.Markdown (markdownSupport)
+import Xrefcheck.System (askWithinCI)
+import Xrefcheck.Verify (verifyErrors, verifyRepo)
+
+readConfig :: FilePath -> IO Config
+readConfig path =
+  decodeFileEither path
+  >>= either (error . toText . prettyPrintParseException) pure
+
+formats :: ScannersConfig -> FormatsSupport
+formats ScannersConfig{..} = specificFormatsSupport
+    [ markdownSupport scMarkdown
+    ]
+
+findFirstExistingFile :: [FilePath] -> IO (Maybe FilePath)
+findFirstExistingFile = \case
+  [] -> pure Nothing
+  (file : files) -> do
+    exists <- doesFileExist file
+    if exists then pure (Just file) else findFirstExistingFile files
+
+defaultAction :: Options -> IO ()
+defaultAction Options{..} = do
+    config <- case oConfigPath of
+      Just configPath -> readConfig configPath
+      Nothing -> do
+        mConfigPath <- findFirstExistingFile defaultConfigPaths
+        case mConfigPath of
+          Just configPath -> readConfig configPath
+          Nothing -> do
+            hPutStrLn @Text stderr
+              "Configuration file not found, using default config \
+              \for GitHub repositories\n"
+            pure $ defConfig GitHub
+
+    withinCI <- askWithinCI
+    let showProgressBar = oShowProgressBar ?: not withinCI
+
+    repoInfo <- allowRewrite showProgressBar $ \rw -> do
+      let fullConfig = addTraversalOptions (cTraversal config) oTraversalOptions
+      gatherRepoInfo rw (formats $ cScanners config) fullConfig oRoot
+
+    when oVerbose $
+      fmtLn $ "=== Repository data ===\n\n" <> indentF 2 (build repoInfo)
+
+    verifyRes <- allowRewrite showProgressBar $ \rw -> do
+      let fullConfig = addVerifyOptions (cVerification config) oVerifyOptions
+      verifyRepo rw fullConfig oMode oRoot repoInfo
+
+    case verifyErrors verifyRes of
+      Nothing ->
+        fmtLn "All repository links are valid."
+      Just (toList -> errs) -> do
+        fmt $ "=== Invalid references found ===\n\n" <>
+              indentF 2 (blockListF' "➥ " build errs)
+        fmtLn $ "Invalid references dumped, " <> build (length errs) <> " in total."
+        exitFailure
